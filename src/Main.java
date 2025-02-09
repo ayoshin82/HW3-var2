712b4e90705efb110490858926727af1164ef767
ayoshin82 <ayoshin82@.gmail.com>
2025-02-09 09:28:58 +0700

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание № 3");
+
+        System.out.println("Задача № 1");
+
+        int a = 45000;
+        byte  b= 127;
+        short c = 3277;
+        long d = 400000000L;
+        float f = 3.7500021f;
+        double j = 3.5786999123456789;
+        System.out.println("Значение переменной a с типом int равно " + a );
+        System.out.println("Значение переменной b с типом  byte  равно " + b );
+        System.out.println("Значение переменной  c с типом  short   равно " + c  );
+        System.out.println("Значение переменной  d с типом  long   равно  " + d);
+        System.out.println("Значение переменной  f с типом   float    равно  " + f);
+        System.out.println("Значение переменной  j с типом  double   равно  " + j );
+    }
+}
